4b28e6a31cd0b9e043d72f1901ed5b2a30c72fea
shyeon0713 <tmdgus2086@naver.com>
2025-11-24 18:21:01 +0900

    20251124
    1. 음악 선택 UI 수정
    2. UI 선택 바 필요

---
+++ Assets/Script/MusicData/UI/SongListUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+
+// 선택한 곡 리스트를 보여주기 위한 컴포넌트 UI코드 
+// 곡을 선택할 때, 해당 에셋의 텍스트 및 이미지 내용을 변경하는 방식이 아닌
+// 선택영역을 지정해두고, 버튼 리스트가 이동하는 방식으로 UI 제작 -> 새로운 방식으로 버튼 선택 UI를 구현하고 싶었음.
+public class SongListUI : MonoBehaviour
+{
+    public SongEntry song;     // 이 버튼이 들고 있는 곡
+    public TMP_Text title; // 버튼에 표시될 곡 제목
+
+    public void Setup(SongEntry songEntry)
+    {
+        song = songEntry;
+
+        if (title != null)  // 타이틀 정보가 있을 경우.
+            title.text = songEntry.title;  //타이틀 정보 출력
+    }
+}
--- Assets/Script/MusicData/UI/SongSelectUI.cs
+++ Assets/Script/MusicData/UI/SongSelectUI.cs
@@ -1,40 +1,121 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SongSelectUI: MonoBehaviour
 {
     public MusicDatabase database;
     //SongEntry 리스트 기반으로 UI 버튼 목록 생성
 
-    public GameObject songbutpre;
-    // 노래 목록 버튼 프리팹
+    [Header("노래 선택 버튼 프리팹 및 정렬 설정")]
+    public GameObject songbutpre; // 노래 목록 버튼 프리팹
+    public Transform songListRoot;  // 버튼들이 정렬될 부모 컴포넌트
 
-    public Transform songListRoot;
-    //버튼 위치 배치
+    [Header("노래 리스트 선택")]
+    public TMP_Text songtitle;
+    public Button prebut;  // 곡 선택 버튼 ->이전
+    public Button nextbut;  //곡 선택 버튼 -> 이후
+
+    private int currentIndex = 0;  // 곡 선택 index ,초기 0번으로 설정
 
     public SelectDifficultyUI difficultyUI;
-    // 난이도 선택 스트립트 참조
+    // 난이도 선택 UI
 
     private void Start()
     {
-        CreatSongButtons();
+        // 노래 리스트 존재하는지 디버깅 확인
+        if (database == null || database.songs == null || database.songs.Count == 0)
+        {
+            Debug.LogError("[SongSelectUI] MusicDatabase가 비어 있습니다.");
+            return;
+        }
+
+        CreatSongButtons();  // 노래 리스트 버튼 배치
+
+        prebut.onClick.AddListener(PreSongselect);  // 이전 버튼 리스너 추가
+        nextbut.onClick.AddListener(NextSongselect); //다음 버튼 리스너 추가
+
+        UpdateSelectedSongUI();  // 중앙 영역에 있는 곡 정보 갱신
     }
 
+
+
+    #region - 리스너 연결 -> 버튼에 따라 노래 리스트 버튼 이동
+    void PreSongselect()
+    {
+        int count = songListRoot.childCount;   // .childCount -> 현재 부모가 가지고 있는 자식 개수 
+        if (count <= 1) return;
+
+        Transform last = songListRoot.GetChild(count - 1);   // songListRoot의 count -1 번째 인덱스의 자식을 가져오는 함수
+        last.SetSiblingIndex(0);  // SetSiblingIndex( ) : 부모의 자식 컴포넌트 순서를 바꾸는 함수
+        // 0번 인덱스를 맨 뒤로 이동
+
+        UpdateSelectedSongUI();
+    }
+
+    void NextSongselect()
+    {
+        int count = songListRoot.childCount;
+        if(count <= 1) return;
+
+        Transform first = songListRoot.GetChild(0);  //songListRoot의 0번째 인덱스의 자식을 가져오는 함수
+        first.SetSiblingIndex(count - 1);
+        // 마지막 인덱스를 맨 앞으로 이동
+
+        UpdateSelectedSongUI();
+    }
+
+    #endregion
+
+
+    #region - 노래 리스트 버튼 생성 -> SongEntry 갯수에 맞춰 프리팹을 생성 및 배치 
     void CreatSongButtons()
     {
         foreach(var song in database.songs)
         {
-            var btnObj = Instantiate(songbutpre, songListRoot);
-            btnObj.GetComponentInChildren<Text>().text = song.title;
+            var btnObj = Instantiate(songbutpre, songListRoot);  // 리스트 버튼 배치
 
+            var songBtn = btnObj.GetComponent<SongListUI>();
+            if (songBtn != null)
+                songBtn.Setup(song);
+            //추후에 자켓 이미지도 추가하는 방향 고려
+            //
             btnObj.GetComponent<Button>().onClick.AddListener(() =>
             {
-                difficultyUI.Open(song); //곡 하나 선택 → 난이도 창으로 넘기기
+                    difficultyUI.Open(song); //곡 하나 선택 → 난이도 창으로 넘기기
             });
+           
         }
     }
 
+    #endregion
 
+    #region- 선택 영역에 있는 노래 표시 
+    SongEntry GetCenterSong()
+    {
+        int count = songListRoot.childCount;
+        if (count == 0) return null;
 
+        int centerIndex = count / 2;
+        var t = songListRoot.GetChild(centerIndex);
+        var btn = t.GetComponent<SongListUI>();
 
+        return btn != null ? btn.song : null;
+    }
+    #endregion
+
+    #region - 노래 정보 표시
+    void UpdateSelectedSongUI()
+    {
+        var song = GetCenterSong();  // 선택 영역에 있는 노래 선택
+        if (song == null)
+        {
+            if (songtitle != null) songtitle.text = "-";
+            return;
+        }
+
+        if (songtitle != null)
+            songtitle.text = song.title;
+    }
+    #endregion
 }
